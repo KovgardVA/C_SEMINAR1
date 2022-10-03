c2f3e5838c87a66b91db4378994e6674dae622e8
avraamovolono <avraamovolono@yandex.ru>
2022-10-03 22:33:24 +0300

    finish Example 002 Weekday project

--- Example002_Weekday/Program.cs
+++ Example002_Weekday/Program.cs
@@ -57,3 +57,28 @@ else
 {
     Console.WriteLine(days[day - 1]);
 }
+
+// switch (day)
+// {
+//     case 1:
+//         Console.WriteLine("Понедельник");
+//         break;
+//     case 2:
+//         Console.WriteLine("Вторник");
+//         break;
+//     case 3:
+//         Console.WriteLine("Среда");
+//         break;
+//     case 4:
+//         Console.WriteLine("Четверг");
+//         break;
+//     case 5:
+//         Console.WriteLine("Пятница");
+//         break;
+//     case 6:
+//         Console.WriteLine("Суббота");
+//         break;
+//     case 7:
+//         Console.WriteLine("Воскресенье");
+//         break;
+// }
