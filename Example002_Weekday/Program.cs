345f6807ae5a43ae36e073ba940612c35362167b
avraamovolono <avraamovolono@yandex.ru>
2022-10-03 22:15:37 +0300

    add Example 002 Weekday project

---
+++ Example002_Weekday/Program.cs
@@ -0,0 +1,59 @@
+﻿// Найти день недели по введенному номеру
+
+// Console.WriteLine("Введите номер:");
+// string weekday = Console.ReadLine();
+
+// if(weekday == "1")
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if(weekday == "2")
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if(weekday == "3")
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if(weekday == "4")
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if(weekday == "5")
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if(weekday == "6")
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if(weekday == "7")
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Введено неверное значение!");
+// }
+
+Console.WriteLine("Ведите номер дня недели:");
+int day = Convert.ToInt32(Console.ReadLine());
+string[] days =
+{
+    "Понедельник",
+    "Втоник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье"
+};
+
+if (day < 1 || day > 7)
+{
+    Console.WriteLine("Это не день недели!");
+}
+else
+{
+    Console.WriteLine(days[day - 1]);
+}
